f665e3c30daec057214a267c6aef23854a71c186
Drangoht <drangoht@gmail.co>
2025-02-25 21:09:05 +0100

    Add project files.

---
+++ LocalLLM.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.AI;
+
+// docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+// docker exec -it ollama ollama pull llama3.1
+// docker exec -it ollama ollama list
+IChatClient client = 
+    new OllamaChatClient(new Uri("http://localhost:11434/"), "llama3.1");
+
+var response = await client.GetResponseAsync("What is AI?");
+
+Console.WriteLine(response.Message);
---
+++ LocalLLM.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace LocalLLM.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
